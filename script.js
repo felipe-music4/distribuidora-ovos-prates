85e0cbc879d67a329d3bb2d93ef10fa99fa1cdf1
Felipe_music4 <amaral.felipe34@gmail.com>
2025-10-25 12:46:36 -0300

    alteracao do projeto

--- script.js
+++ script.js
@@ -106,3 +106,11 @@ function enviarParaWhatsApp() {
 
   window.open(url, '_blank');
 }
+document.addEventListener("DOMContentLoaded", function () {
+  const toggle = document.querySelector(".menu-toggle");
+  const links = document.querySelector(".menu-links");
+
+  toggle.addEventListener("click", function () {
+    links.style.display = links.style.display === "flex" ? "none" : "flex";
+  });
+});
